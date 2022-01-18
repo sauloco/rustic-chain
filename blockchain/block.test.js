e43f9c87ed6978e6f2049c29438038d73011625d
Saulo Vargas <sauloco022@gmail.com>
2022-01-18 17:01:16 -0300

    Initial commit
    Blockchain base implemented
    API endopoints running

---
+++ blockchain/block.test.js
@@ -0,0 +1,20 @@
+const Block = require('./block');
+
+describe('Block', () => {
+  let data, lastBlock;
+
+  beforeEach(() => {
+    data = 'bar';
+    lastBlock = Block.genesis();
+
+    block = Block.mineBlock(lastBlock, data);
+  });
+  it('set the `data` to match the input', () => {
+    expect(block.data).toEqual(data);
+  });
+
+  it('sets the `lastHash` to match the hash of the last block', () => {
+    expect(block.lastHash).toEqual(lastBlock.hash);
+  });
+
+})
