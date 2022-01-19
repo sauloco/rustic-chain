3887744fe6bdc79b4a9fbcbdfc72128f5f51c2b9
Saulo Vargas <sauloco022@gmail.com>
2022-01-19 10:05:17 -0300

    Sign a transaction

--- chain-util.js
+++ chain-util.js
@@ -1,3 +1,4 @@
+const SHA256 = require("crypto-js/sha256");
 const EC = require('elliptic').ec;
 const { v1: uuidV1 } = require('uuid');
 
@@ -10,6 +11,10 @@ class ChainUtil {
 
   static id() {
     return uuidV1();
+  }
+
+  static hash(data) {
+    return SHA256(JSON.stringify(data)).toString();
   }
 }
 
