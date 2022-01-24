8deaf78d3430c1f341853abd3f01668fd5b55bb6
Saulo Vargas <sauloco022@gmail.com>
2022-01-24 15:08:20 -0300

    grab valid transactions

--- wallet/transaction-pool.js
+++ wallet/transaction-pool.js
@@ -1,3 +1,4 @@
+const Transaction = require('./transaction');
 class TransactionPool {
   constructor(){
     this.transactions = [];
@@ -16,6 +17,22 @@ class TransactionPool {
 
   existingTransaction(publicKey){
     return this.transactions.find(t => t.input.address === publicKey);
+  }
+
+  validTransactions(){
+    return this.transactions.filter(transaction => {
+      const outputTotal = transaction.outputs.reduce((total, output) => total + output.amount, 0)
+
+      if (transaction.input.amount !== outputTotal) {
+        console.log(`Invalid transaction from ${transaction.input.address}.`);
+        return;
+      }
+
+      if (!Transaction.verifyTransaction(transaction)) {
+        console.log(`Invalid signature from ${transaction.input.address}.`);
+        return;
+      }
+    })
   }
 }
 
