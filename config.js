849f35bc65f518831681f57cd18208909cc3a26f
Saulo Vargas <sauloco022@gmail.com>
2022-01-24 15:30:01 -0300

    reward transactions

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 const DIFFICULTY = 4;
 const MINE_RATE = 3000;
 const INITIAL_BALANCE = 500;
+const MINING_REWARD = 50;
+const REWARD_INPUT = { address: '*authorized-reward*' };
 
-module.exports = { DIFFICULTY, MINE_RATE, INITIAL_BALANCE };
+module.exports = { DIFFICULTY, MINE_RATE, INITIAL_BALANCE, MINING_REWARD };
--- wallet/transaction.js
+++ wallet/transaction.js
@@ -1,4 +1,5 @@
 const ChainUtil = require('../chain-util');
+const { MINING_REWARD } = require('../config');
 
 class Transaction {
   constructor() {
@@ -22,6 +23,15 @@ class Transaction {
     return this;
   }
 
+  static transactionWithOutputs(senderWallet, outputs) {
+    const transaction  = new this();
+
+    transaction.outputs.push(...outputs);
+    Transaction.signTransaction(transaction, senderWallet);
+
+    return transaction;
+  }
+
   static newTransaction(senderWallet, recipient, amount) {
     const transaction = new this();
 
@@ -30,7 +40,9 @@ class Transaction {
       return;
     }
 
-    transaction.outputs.push(...[
+    Transaction.signTransaction(transaction, senderWallet);
+
+    return Transaction.transactionWithOutputs(senderWallet, [
       {
         amount: senderWallet.balance - amount,
         address: senderWallet.publicKey
@@ -39,11 +51,13 @@ class Transaction {
         amount,
         address: recipient
       }
-    ]);
+    ])
+  }
 
-    Transaction.signTransaction(transaction, senderWallet);
-
-    return transaction;
+  static rewardTransaction(minerWallet, blockchainWallet) {
+    return Transaction.transactionWithOutputs(blockchainWallet, [{
+      amount: MINING_REWARD, address: minerWallet.publicKey 
+    }]);
   }
 
   static signTransaction(transaction, senderWallet) {
