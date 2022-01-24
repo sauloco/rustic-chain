7caf453a3157c48d9b69ee668f02b3e7091307e8
Saulo Vargas <sauloco022@gmail.com>
2022-01-24 15:54:03 -0300

    mine transactions endpoint

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ const Blockchain = require('../blockchain');
 const P2PServer = require('./p2p-server');
 const Wallet = require('../wallet');
 const TransactionPool = require('../wallet/transaction-pool');
-const Transaction = require('../wallet/transaction');
+const Miner = require('./miner');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
@@ -13,12 +13,20 @@ const bc = new Blockchain();
 const wallet = new Wallet();
 const tp = new TransactionPool();
 const p2pServer = new P2PServer(bc, tp);
+const miner = new Miner(bc, tp, wallet, p2pServer);
 
 app.use(bodyParser.json());
 
 app.get('/blocks', (req, res) => {
   res.json(bc.chain);
 });
+
+app.get('/mine-transactions', (req, res) => {
+  const block = miner.mine();
+  console.log(`New block added: ${block.toString()}`);
+
+  res.redirect('/blocks');
+})
 
 app.post('/mine', (req, res) => {
   const block = bc.addBlock(req.body.data);
